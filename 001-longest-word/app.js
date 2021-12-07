edcd378c8cc33a65c83c9112b44f25d7dc05ee10
Aleksandar Ristic <storiaca@gmail.com>
2021-12-07 19:48:18 +0100

    Longest word advanced solution

--- 001-longest-word/app.js
+++ 001-longest-word/app.js
@@ -17,10 +17,30 @@
 // }
 
 // Intermediate Solution
+
+// function longestWord(str) {
+//   return str.split(" ").reduce(function (x, y) {
+//     return Math.max(x, y.length);
+//   }, 0);
+// }
+
+// Advanced Solution
 function longestWord(str) {
-  return str.split(" ").reduce(function (x, y) {
-    return Math.max(x, y.length);
-  }, 0);
+  str = str.split(" ");
+
+  if (str.length == 1) {
+    return str[0].length;
+  }
+
+  if (str[0].length >= str[1].length) {
+    str.splice(1, 1);
+    return longestWord(str.join(" "));
+  }
+
+  if (str[0].length <= str[1].length) {
+    return longestWord(str.slice(1, str.length).join(" "));
+  }
+  return str.length;
 }
 console.log(longestWord("The quick brown fox jumped over the lazy dog"));
 console.log(
