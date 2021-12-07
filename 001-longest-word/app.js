5804272d54f361722d68f4800ec81bc72943bc01
Aleksandar Ristic <storiaca@gmail.com>
2021-12-07 19:37:15 +0100

    Longest word intermediate solution

--- 001-longest-word/app.js
+++ 001-longest-word/app.js
@@ -4,17 +4,24 @@
   Ex. "What if we try a super-long word such as otorhinolaryngology"
 */
 // Basic Solution
-function longestWord(str) {
-  const words = str.split(" ");
-  let longestWord = 0;
-  for (let i = 0; i < words.length; i++) {
-    if (words[i].length > longestWord) {
-      longestWord = words[i].length;
-    }
-  }
-  return longestWord;
-}
 
+// function longestWord(str) {
+//   const words = str.split(" ");
+//   let longestWord = 0;
+//   for (let i = 0; i < words.length; i++) {
+//     if (words[i].length > longestWord) {
+//       longestWord = words[i].length;
+//     }
+//   }
+//   return longestWord;
+// }
+
+// Intermediate Solution
+function longestWord(str) {
+  return str.split(" ").reduce(function (x, y) {
+    return Math.max(x, y.length);
+  }, 0);
+}
 console.log(longestWord("The quick brown fox jumped over the lazy dog"));
 console.log(
   longestWord("What if we try a super-long word such as otorhinolaryngology")
