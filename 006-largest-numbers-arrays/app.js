db2a580a1db7984c1545dc067a00aba887d91fa1
Software/Nation <storiaca@gmail.com>
2021-12-15 16:52:06 +0100

    Largest number in array start

---
+++ 006-largest-numbers-arrays/app.js
@@ -0,0 +1,15 @@
+/* 
+  Return Largest Numbers in Arrays
+  array will have 583, 999, 113, 55
+*/
+
+const numArray = [
+  [583, 999, 113, 55],
+  [600, 187, 798, 444],
+  [1023, 77, 832, 285],
+  [83, 99, 113, 555],
+];
+// Basic Solution
+function largestOfFour(arr) {}
+
+//console.log(titleCase("I'm a little tea pot"));
