644d75ce5c777fed216431622c2ba8261938e3cc
Aleksandar Ristic <storiaca@gmail.com>
2021-12-16 17:42:20 +0100

    Largest number basic colution

--- 006-largest-numbers-arrays/app.js
+++ 006-largest-numbers-arrays/app.js
@@ -10,6 +10,18 @@ const numArray = [
   [83, 99, 113, 555],
 ];
 // Basic Solution
-function largestOfFour(arr) {}
+function largestOfFour(arr) {
+  var results = [];
+  for (let n = 0; n < arr.length; n++) {
+    const largestNumber = arr[n][0];
+    for (let sb = 1; sb < arr[n].length; sb++) {
+      if (arr[n][sb] > largestNumber) {
+        largestNumber = arr[n][sb];
+      }
+    }
+    results[n] = largestNumber;
+  }
+  return results;
+}
 
 //console.log(titleCase("I'm a little tea pot"));
