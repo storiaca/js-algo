6dc2b451047323fa6df261d1582f000311ff96f2
Aleksandar Ristic <storiaca@gmail.com>
2021-12-18 17:46:44 +0100

    Largest number in arrays finished

--- 006-largest-numbers-arrays/app.js
+++ 006-largest-numbers-arrays/app.js
@@ -10,18 +10,35 @@ const numArray = [
   [83, 99, 113, 555],
 ];
 // Basic Solution
+// function largestOfFour(arr) {
+//   let results = [];
+//   for (let n = 0; n < arr.length; n++) {
+//     let largestNumber = arr[n][0];
+//     for (let sb = 1; sb < arr[n].length; sb++) {
+//       if (arr[n][sb] > largestNumber) {
+//         largestNumber = arr[n][sb];
+//       }
+//     }
+//     results[n] = largestNumber;
+//   }
+//   return results;
+// }
+
+// Intermediate Solution
+// function largestOfFour(arr) {
+//   return arr.map(function (group) {
+//     return group.reduce((prev, curr) => {
+//       return curr > prev ? curr : prev;
+//     }, 0);
+//   });
+// }
+
+// Advanced Solution
 function largestOfFour(arr) {
-  var results = [];
-  for (let n = 0; n < arr.length; n++) {
-    const largestNumber = arr[n][0];
-    for (let sb = 1; sb < arr[n].length; sb++) {
-      if (arr[n][sb] > largestNumber) {
-        largestNumber = arr[n][sb];
-      }
-    }
-    results[n] = largestNumber;
-  }
-  return results;
+  return arr.map(Function.apply.bind(Math.max, null));
 }
 
-//console.log(titleCase("I'm a little tea pot"));
+// function largestOfFour(arr) {
+//   return arr.map((i) => Math.max(...i));
+// }
+console.log(largestOfFour(numArray));
