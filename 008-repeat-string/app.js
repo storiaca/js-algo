20df5964509088fbcc7d8f32b3d8fe071bb95bad
Aleksandar Ristic <storiaca@gmail.com>
2021-12-22 08:39:18 +0100

    Repeat a string

--- 008-repeat-string/app.js
+++ 008-repeat-string/app.js
@@ -4,11 +4,23 @@
 */
 // Basic Solution
 
+// function repeatStringNumTimes(str, num) {
+//   if (num > 0) {
+//     return str.repeat(num);
+//   }
+//   return "";
+// }
+
+// Intermediate Solution
+// function repeatStringNumTimes(str, num) {
+//   if (num < 0) return "";
+//   if (num === 1) return str;
+//   else return str + repeatStringNumTimes(str, num - 1);
+// }
+
+// Advanced Solution
 function repeatStringNumTimes(str, num) {
-  if (num > 0) {
-    return str.repeat(num);
-  }
-  return "";
+  return num > 0 ? str.repeat(num) : "";
 }
 
 console.log(repeatStringNumTimes("candy apple", -1));
